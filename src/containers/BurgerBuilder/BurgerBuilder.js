5cd770d1c8eee1403f9882f9994b3060fac3a0ec
Rudra <rudra.kaniya.rk@gmail.com>
2021-02-22 00:17:40 +0530

    Adding the checkOut page,

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -97,31 +97,33 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert("Welcome welcome welcome!!!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Rudra Kaniya",
-        address: {
-          street: "Shant Street",
-          zipCode: "100010",
-          country: "Switzerland",
-        },
-        email: "rudra.kaniya.rk@gmail.com",
-      },
-      deliveryMethod: "fastest",
-    };
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Rudra Kaniya",
+    //     address: {
+    //       street: "Shant Street",
+    //       zipCode: "100010",
+    //       country: "Switzerland",
+    //     },
+    //     email: "rudra.kaniya.rk@gmail.com",
+    //   },
+    //   deliveryMethod: "fastest",
+    // };
 
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        console.log(response);
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // axios
+    //   .post("/orders.json", order)
+    //   .then((response) => {
+    //     console.log(response);
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+
+    this.props.history.push("/checkout");
   };
 
   render() {
